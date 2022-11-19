05452efc1a3710193dbbbb96bb3e8abde748d4c4
Aditi <shrivastvaaditi@gmail.com>
2022-11-19 22:13:29 +0530

    upload demo

---
+++ src/com/semanticsquare/exceptions/APIFormatChangeException.java
@@ -0,0 +1,10 @@
+package com.semanticsquare.exceptions;
+
+public class APIFormatChangeException extends Exception {
+
+	public APIFormatChangeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+    
+}
